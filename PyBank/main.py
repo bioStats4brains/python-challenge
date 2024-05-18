c1b1a9ff5b9ae6554623d4a8262ef2643a4b5adf
krissyBios <123996720+bioStats4brains@users.noreply.github.com>
2024-05-18 11:54:07 -0700

    update all the files

---
+++ PyBank/main.py
@@ -0,0 +1,8 @@
+# Import the os module
+import os
+
+# Module for reading CSV files
+import csv
+
+# Read in data 
+csvpath = os.path.join('/Users/dkknight/python-challenge/PyBank/Resources/budget_data.csv')

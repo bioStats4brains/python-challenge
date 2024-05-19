f87f6902381efe7e271c33cfa0520309c0799995
krissyBios <123996720+bioStats4brains@users.noreply.github.com>
2024-05-18 18:31:52 -0700

    Save the print statements

---
+++ PyBank/.ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,9 @@
+# Import the os module
+import os
+
+# Module for reading CSV files
+import csv
+
+# Read in data 
+file_path = open('PyBank/Resources/budget_data.csv')
+type(file_path)
--- PyBank/main.py
+++ PyBank/main.py
@@ -1,8 +1,35 @@
-# Import the os module
+
+# Welcome message to test the output! 
+msg = "Enter into the world of coding!"
+print(msg)
+
+# Import dependencies.
 import os
-
-# Module for reading CSV files
 import csv
+import pandas as pd
 
-# Read in data 
-csvpath = os.path.join('/Users/dkknight/python-challenge/PyBank/Resources/budget_data.csv')
+# Read in data. 
+budget_df = pd.read_csv("PyBank/Resources/budget_data.csv")
+
+print(budget_df.head())
+
+#The total number of months included in the dataset.
+num_months = len(pd.unique(budget_df["Date"]))
+
+print("The total number of months is :", num_months)
+
+#The net amount of profit/losses. 
+profit = budget_df.loc[budget_df["Profit/Losses"] > 0, 'Profit/Losses'].sum()
+losses = budget_df.loc[budget_df["Profit/Losses"] < 0, 'Profit/Losses'].sum()
+net_profit = profit + losses 
+
+print("The net profit/losses is ", net_profit, ".")
+
+#The greatest increase/decrease in profits happened on __ and for __ much. 
+max_date = budget_df.loc[max(budget_df["Profit/Losses"]), 'Date']
+max_profit = budget_df.loc[max(budget_df["Profit/Losses"]), 'Profit/Losses']
+min_date = budget_df.loc[min(budget_df["Profit/Losses"]), 'Date']
+min_profit = budget_df.loc[min(budget_df["Profit/Losses"]), 'Profit/Losses']
+
+print("The greatest increase in profits happened on: ", max_date, " (", max_profit, ").")
+print("The greatest decrease in profits happened on: ", min_date, " (", min_profit, ").")

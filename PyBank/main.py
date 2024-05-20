f32733aae8dac76702b762c019677dd3dcb7a6e5
krissyBios <123996720+bioStats4brains@users.noreply.github.com>
2024-05-20 12:54:26 -0700

    print out statemnet

--- PyBank/main.py
+++ PyBank/main.py
@@ -23,23 +23,14 @@ profit = budget_df.loc[budget_df["Profit/Losses"] > 0, 'Profit/Losses'].sum()
 losses = budget_df.loc[budget_df["Profit/Losses"] < 0, 'Profit/Losses'].sum()
 net_profit = profit + losses 
 
-print("The net profit/losses is ", net_profit, ".")
+print("The net profit/losses is $", net_profit, ".")
 
 #The greatest increase/decrease in profits happened on __ and for __ much. 
-#max_date = budget_df['Profit/Losses'].idxmax()
 max_date = budget_df.loc[budget_df["Profit/Losses"].idxmax(), 'Date']
 max_profit = budget_df.loc[budget_df["Profit/Losses"].idxmax(), 'Profit/Losses']
-print("The greatest increase in profits happened on: ", max_date, " (", max_profit, ").")
+print("The greatest increase in profits happened on: ", max_date, " ($", max_profit, ").")
 
+min_date = budget_df.loc[budget_df["Profit/Losses"].idxmin(), 'Date']
+min_profit = budget_df.loc[budget_df["Profit/Losses"].idxmin(), 'Profit/Losses']
+print("The greatest decrease in profits happened on: ", min_date, " ($", min_profit, ").")
 
-#print("The greatest increase in profits happened on: ", max_date)
-
-
-#max_date = budget_df.loc[(max(budget_df["Profit/Losses"])), ['Profit/Losses']]
-#print("The greatest increase in profits happened on: ", max_date)
-#max_profit = budget_df.loc[max(budget_df["Profit/Losses"]), 'Profit/Losses']
-#min_date = budget_df.loc[min(budget_df["Profit/Losses"]), 'Date']
-#min_profit = budget_df.loc[min(budget_df["Profit/Losses"]), 'Profit/Losses']
-
-#print("The greatest increase in profits happened on: ", max_date)# " (", max_profit, ").")
-#print("The greatest decrease in profits happened on: ", min_date, " (", min_profit, ").")

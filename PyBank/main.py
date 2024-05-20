465c7a625decefa3fbc70d1663f82385a3546b40
krissyBios <123996720+bioStats4brains@users.noreply.github.com>
2024-05-20 12:58:38 -0700

    print to txt file

--- PyBank/main.py
+++ PyBank/main.py
@@ -16,21 +16,23 @@ print(budget_df.head())
 #The total number of months included in the dataset.
 num_months = len(pd.unique(budget_df["Date"]))
 
-print("The total number of months is :", num_months)
+f = open("output_print.txt", "a")
+print("The total number of months is :", num_months, file=f)
 
 #The net amount of profit/losses. 
 profit = budget_df.loc[budget_df["Profit/Losses"] > 0, 'Profit/Losses'].sum()
 losses = budget_df.loc[budget_df["Profit/Losses"] < 0, 'Profit/Losses'].sum()
 net_profit = profit + losses 
 
-print("The net profit/losses is $", net_profit, ".")
+print("The net profit/losses is $", net_profit, ".", file=f)
 
 #The greatest increase/decrease in profits happened on __ and for __ much. 
 max_date = budget_df.loc[budget_df["Profit/Losses"].idxmax(), 'Date']
 max_profit = budget_df.loc[budget_df["Profit/Losses"].idxmax(), 'Profit/Losses']
-print("The greatest increase in profits happened on: ", max_date, " ($", max_profit, ").")
+print("The greatest increase in profits happened on: ", max_date, " ($", max_profit, ").", file=f)
 
 min_date = budget_df.loc[budget_df["Profit/Losses"].idxmin(), 'Date']
 min_profit = budget_df.loc[budget_df["Profit/Losses"].idxmin(), 'Profit/Losses']
-print("The greatest decrease in profits happened on: ", min_date, " ($", min_profit, ").")
+print("The greatest decrease in profits happened on: ", min_date, " ($", min_profit, ").", file=f)
 
+f.close()
